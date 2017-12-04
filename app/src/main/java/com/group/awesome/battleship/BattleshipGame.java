b46cac2f5a587eb97de2a8a56489505c76248c1e
Jacob <jacob.egebjerg@gmail.com>
2017-12-04 15:00:17 +0100

    Added a Game and supporting classes.

---
+++ app/src/main/java/com/group/awesome/battleship/BattleshipGame.java
@@ -0,0 +1,22 @@
+package com.group.awesome.battleship;
+
+/**
+ * Created by Jacob on 12/4/2017.
+ */
+
+public class BattleshipGame {
+
+    // 0 - empty not hit
+    // 1 - empty hit
+    // 2 - ship not hit
+    // 3 - ship hit
+    public int grid1[][];
+    public int grid2[][];
+
+    public BattleshipGame(){
+
+    }
+
+
+
+}
---
+++ app/src/main/java/com/group/awesome/battleship/XYCoordinate.java
@@ -0,0 +1,10 @@
+package com.group.awesome.battleship;
+
+/**
+ * Created by Jacob on 12/4/2017.
+ */
+
+public class XYCoordinate {
+    public int x;
+    public int y;
+}
